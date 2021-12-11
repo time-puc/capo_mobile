a3c2ff97dcd212b5ef0997862f71fedf975d0569
amorimcode <bruno.amorim032@gmail.com>
2021-12-11 13:00:00 -0300

    scroll na tela

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -8,34 +8,50 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    labelText: 'E-mail', border: OutlineInputBorder()),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    labelText: 'Senha', border: OutlineInputBorder()),
-              ),
-              SizedBox(height: 10),
-              RaisedButton(
-                onPressed: () {},
-                child: Text('Entrar'),
-              )
-            ],
+      child: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  onChanged: (text) {
+                    email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'E-mail', border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  onChanged: (text) {
+                    password = text;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: 'Senha', border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 10),
+                RaisedButton(
+                  onPressed: () {
+                    if (email == 'bruno.as4@puccampinas.edu.br' &&
+                        password == '123') {
+                      print('correto');
+                    }
+                  },
+                  child: Text('Entrar'),
+                )
+              ],
+            ),
           ),
         ),
       ),
